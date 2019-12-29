d3b7cef6dfd1a8aa83d261b2066582003edc651b
harjoth <harjoth.khara@gmail.com>
2019-12-28 17:57:52 -0800

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <div className='App'>
-      <h1>MarioPlan</h1>
+      <Navbar />
     </div>
   );
 }
