a77c6838c4f46b967c6b6a5a4779b6d1a37083ab
harjoth <harjoth.khara@gmail.com>
2019-12-30 13:10:49 -0800

    adding dummy data and outputting to the DOM using the redux store

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
+import { connect } from 'react-redux';
 
 class Dashboard extends Component {
   render() {
+    // console.log(this.props);
+    const { projects } = this.props;
     return (
       <div className='dashboard container'>
         <div className='row'>
           <div className='col s12 m6'>
-            <ProjectList />
+            <ProjectList projects={projects} />
           </div>
           <div className='col s12 m5 offset-m1'>
             <Notifications />
@@ -18,5 +21,11 @@ class Dashboard extends Component {
     );
   }
 }
-
-export default Dashboard;
+// mapping state from our redux store to the props of this component
+const mapStateToProps = state => {
+  return {
+    projects: state.project.projects
+  };
+};
+// connect is a HOC that returns this component(Dashboard) supercharged with props from our store.
+export default connect(mapStateToProps)(Dashboard);
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ProjectSummary from './ProjectSummary';
 
-const ProjectList = () => {
+const ProjectList = ({ projects }) => {
+  // console.log(props);
   return (
     <div className='project-list section'>
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
+      {/* if we have projects then map, otherwise don't do anything. we do this to make sure we have something to map over, especially if we're reaching out to an external database */}
+      {projects &&
+        projects.map(project => {
+          return <ProjectSummary project={project} key={project.id} />;
+        })}
     </div>
   );
 };
