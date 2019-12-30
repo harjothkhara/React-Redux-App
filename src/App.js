6dd931203a541a99e39c5475e6aa7d93506dabb6
harjoth <harjoth.khara@gmail.com>
2019-12-30 09:28:36 -0800

    Adding Redux and Reducers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
@@ -9,16 +9,18 @@ import CreateProject from './components/projects/CreateProject';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <Switch>
-        <Route exact path='/' component={Dashboard} />
-        <Route path='/project/:id' component={ProjectDetails} />
-        <Route path='/signin' component={SignIn} />
-        <Route path='/signup' component={SignUp} />
-        <Route path='/create' component={CreateProject} />
-      </Switch>
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/create' component={CreateProject} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Router>
+  <Provider store={store}>
     <App />
-  </Router>,
+  </Provider>
   document.getElementById('root')
 );
 
