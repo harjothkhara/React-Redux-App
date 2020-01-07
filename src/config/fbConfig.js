3d1cd7609c13243b0396a7e1a3382576e1fd41c6
harjoth <harjoth.khara@gmail.com>
2020-01-07 13:50:34 -0800

    setup firebase project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyCOmM0iV4F-eCgtkUpf_sIo6pj_T2-I3z4',
+  authDomain: 'harjoth-marioplan.firebaseapp.com',
+  databaseURL: 'https://harjoth-marioplan.firebaseio.com',
+  projectId: 'harjoth-marioplan',
+  storageBucket: 'harjoth-marioplan.appspot.com',
+  messagingSenderId: '257629511023',
+  appId: '1:257629511023:web:1d5e7b6538da6430b29c9a',
+  measurementId: 'G-GPLMG70S0V'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// initialise database
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
