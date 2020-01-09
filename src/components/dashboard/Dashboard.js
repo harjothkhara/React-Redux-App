1ff6897fad4dcbe302d302e5ebcc9d1cdaba838b
harjoth <harjoth.khara@gmail.com>
2020-01-08 18:55:36 -0800

    synching data with firestore

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 class Dashboard extends Component {
   render() {
@@ -23,9 +25,13 @@ class Dashboard extends Component {
 }
 // mapping state from our redux store to the props of this component
 const mapStateToProps = state => {
+  console.log(state);
   return {
-    projects: state.project.projects
+    projects: state.firestore.ordered.projects
   };
 };
 // connect is a HOC that returns this component(Dashboard) supercharged with props from our store.
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: 'projects' }])
+)(Dashboard);
